484ca84e0020bf5bdeb2e4bc75f04e6fb620d555
Melledy <121644117+Melledy@users.noreply.github.com>
2025-11-14 07:55:06 -0800

    Clone response when getting friends list
    This is so we dont add nextpackages to our cached proto

--- src/main/java/emu/nebula/server/handlers/HandlerFriendListGetReq.java
+++ src/main/java/emu/nebula/server/handlers/HandlerFriendListGetReq.java
@@ -10,8 +10,11 @@ public class HandlerFriendListGetReq extends NetHandler {
 
     @Override
     public byte[] handle(GameSession session, byte[] message) throws Exception {
-        // Build response
-        var rsp = session.getPlayer().getFriendList().toProto();
+        // Get cached friends list proto
+        var proto = session.getPlayer().getFriendList().toProto();
+        
+        // Clone response from cached proto
+        var rsp = proto.clone();
         
         // Encode and send
         return session.encodeMsg(NetMsgId.friend_list_get_succeed_ack, rsp);
